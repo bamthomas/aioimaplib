d54a31201ab3116fa1ce77f0d5a32931814142c7
Bruno Thomas <bruno@barreverte.fr>
2017-07-11 13:45:28 +0200

    [revert] example.py

--- aioimaplib/tests/example.py
+++ aioimaplib/tests/example.py
@@ -1,15 +1,8 @@
 # -*- coding: utf-8 -*-
 import asyncio
 
-import logging
-
 from aioimaplib import aioimaplib
 
-aioimaplib.log.setLevel(logging.DEBUG)
-sh = logging.StreamHandler()
-sh.setLevel(logging.DEBUG)
-sh.setFormatter(logging.Formatter("%(asctime)s %(levelname)s [%(module)s:%(lineno)d] %(message)s"))
-aioimaplib.log.addHandler(sh)
 
 @asyncio.coroutine
 def wait_for_new_message(host, user, password):
@@ -32,20 +25,17 @@ def wait_for_new_message(host, user, password):
 
 @asyncio.coroutine
 def fetch_mail(host, user, password):
-    imap_client = aioimaplib.IMAP4_SSL(host=host, timeout=30)
+    imap_client = aioimaplib.IMAP4_SSL(host=host)
     yield from imap_client.wait_hello_from_server()
 
     yield from imap_client.login(user, password)
 
-    # response = yield from imap_client.select('Draft')
-
-    yield from imap_client.list('""', '%')
+    response = yield from imap_client.select()
+    print('there is %s messages INBOX' % aioimaplib.extract_exists(response))
 
     yield from imap_client.logout()
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    # loop.run_until_complete(fetch_mail('imap-mail.outlook.com', 'tobias.nuxung@outlook.fr', 'TBy2kiagy'))
-    # loop.run_until_complete(fetch_mail('imap.mail.yahoo.com', 'paulmercier75@yahoo.com', 'demonstration'))
-    loop.run_until_complete(fetch_mail('imap.gmail.com', 'thomasbam@gmail.com', 'y2k5iagy'))
+    loop.run_until_complete(fetch_mail('my.imap.server', 'user', 'pass'))
