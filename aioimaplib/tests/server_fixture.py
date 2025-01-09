7f356613308f7d5c02ff545202a4bc30d866d99e
Bruno Thomas <bruno@barreverte.fr>
2025-01-09 18:17:22 +0100

    refactor: test for python 11
    Co-authored-by: Marc Bouvier <marc-bouvier@users.noreply.github.com>

--- aioimaplib/tests/server_fixture.py
+++ aioimaplib/tests/server_fixture.py
@@ -26,23 +26,32 @@ def with_ssl():
     return ssl_context_server, _cert_file
 
 
+@pytest.fixture
+def advance_time(event_loop, request):
+    return EventLoopClockAdvancer(event_loop)
+
+
 @pytest.fixture()
-def with_server(event_loop, capabilities=None):
+def with_server(event_loop, request):
+    capabilities = request.param if hasattr(request, 'param') else None  #
+    imapserver = create_server(capabilities, event_loop)
+    cancel_handle = asyncio.ensure_future(main_test(server=imapserver, ssl_context=None))
+    event_loop.run_until_complete(asyncio.sleep(0.01))
+    try:
+        yield imapserver
+    finally:
+        imapserver.reset()
+        cancel_handle.cancel()
+
+
+def create_server(capabilities, event_loop):
     add_charset('utf-8', SHORTEST, None, 'utf-8')
     add_charset('cp1252', SHORTEST, None, 'cp1252')
     if capabilities is not None:
         imapserver = MockImapServer(loop=event_loop, capabilities=capabilities)
     else:
         imapserver = MockImapServer(loop=event_loop)
-    cancel_handle = asyncio.ensure_future(main_test(server=imapserver, ssl_context=None))
-    event_loop.run_until_complete(asyncio.sleep(0.01))
-
-    try:
-        yield imapserver
-    finally:
-        cancel_handle.cancel()
-
-    imapserver.reset()
+    return imapserver
 
 
 @pytest.fixture()
@@ -76,10 +85,55 @@ async def login_user(login, password, select=False, lib=imaplib.IMAP4, loop=None
     return imap_client
 
 
-async def login_user_async(login, password, select=False):
-    imap_client = aioimaplib.IMAP4(host='127.0.0.1', port=12345, loop=asyncio.get_running_loop())
+async def login_user_async(login, password, select=False, loop=None, timeout=1):
+    test_loop = loop if loop is not None else asyncio.get_running_loop()
+    imap_client = aioimaplib.IMAP4(host='127.0.0.1', port=12345, loop=test_loop, timeout=timeout)
     await imap_client.wait_hello_from_server()
     await imap_client.login(login, password)
     if select:
         await imap_client.select()
     return imap_client
+
+
+class EventLoopClockAdvancer:
+    """
+    from https://github.com/pytest-dev/pytest-asyncio/pull/113
+    A helper object that when called will advance the event loop's time. If the
+    call is awaited, the caller task will wait an iteration for the update to
+    wake up any awaiting handlers.
+    """
+
+    __slots__ = ("offset", "loop", "sleep_duration", "_base_time")
+
+    def __init__(self, loop, sleep_duration=1e-6):
+        self.offset = 0.0
+        self._base_time = loop.time
+        self.loop = loop
+        self.sleep_duration = sleep_duration
+
+        # incorporate offset timing into the event loop
+        self.loop.time = self.time
+
+    def time(self):
+        """
+        Return the time according to the event loop's clock. The time is
+        adjusted by an offset.
+        """
+        return self._base_time() + self.offset
+
+    async def __call__(self, seconds):
+        """
+        Advance time by a given offset in seconds. Returns an awaitable
+        that will complete after all tasks scheduled for after advancement
+        of time are proceeding.
+        """
+        # sleep so that the loop does everything currently waiting
+        await asyncio.sleep(self.sleep_duration)
+
+        if seconds > 0:
+            # advance the clock by the given offset
+            self.offset += seconds
+
+            # Once the clock is adjusted, new tasks may have just been
+            # scheduled for running in the next pass through the event loop
+            await asyncio.sleep(self.sleep_duration)
---
+++ aioimaplib/tests/test_aioimaplib_clocked.py
@@ -0,0 +1,198 @@
+import asyncio
+
+import pytest
+
+from aioimaplib import Command, CommandTimeout, STOP_WAIT_SERVER_PUSH, AioImapException, Response
+from aioimaplib.tests import imapserver
+from aioimaplib.tests.server_fixture import with_server, login_user_async, advance_time
+
+
+@pytest.mark.asyncio()
+async def test_command_timeout(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=1)
+    await advance_time(2)
+    with pytest.raises(AioImapException):
+        await cmd.wait()
+
+
+@pytest.mark.asyncio()
+async def test_command_close_cancels_timer(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=1)
+    cmd.close(b'line', 'OK')
+    await advance_time(3)
+
+    await cmd.wait()
+    assert Response('OK', [b'line']) == cmd.response
+
+
+@pytest.mark.asyncio()
+async def test_command_begin_literal_data_resets_timer(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=2)
+
+    await advance_time(1)
+    cmd.begin_literal_data(7, b'literal')
+
+    await advance_time(1.9)
+    cmd.close(b'line', 'OK')
+
+    await cmd.wait()
+    assert Response('OK', [b'literal', b'line']) == cmd.response
+
+
+@pytest.mark.asyncio()
+async def test_command_append_data_resets_timer(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=2)
+    cmd.begin_literal_data(4, b'da')
+
+    await advance_time(1.9)
+    cmd.append_literal_data(b'ta')
+
+    await advance_time(1.9)
+    cmd.close(b'line', 'OK')
+
+    await cmd.wait()
+    assert Response('OK', [b'data', b'line']) == cmd.response
+
+
+@pytest.mark.asyncio()
+async def test_command_append_literal_data_resets_timer(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=2)
+    cmd.begin_literal_data(12, b'literal')
+
+    await advance_time(1.9)
+    cmd.append_literal_data(b' data')
+
+    await advance_time(1.9)
+    cmd.close(b'line', 'OK')
+
+    await cmd.wait()
+    assert Response('OK', [b'literal data', b'line']) == cmd.response
+
+
+@pytest.mark.asyncio()
+async def test_command_append_to_resp_resets_timer(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=2)
+
+    await advance_time(1.9)
+    cmd.append_to_resp(b'line 1')
+
+    await advance_time(1.9)
+    cmd.close(b'line 2', 'OK')
+
+    await cmd.wait()
+    assert Response('OK', [b'line 1', b'line 2']) == cmd.response
+
+
+@pytest.mark.asyncio()
+async def test_command_timeout_while_receiving_data(event_loop, with_server, advance_time):
+    cmd = Command('CMD', 'tag', loop=event_loop, timeout=2)
+
+    await advance_time(1)
+    cmd.begin_literal_data(12, b'literal')
+
+    await advance_time(3)
+    with pytest.raises(AioImapException):
+        await cmd.wait()
+
+
+@pytest.mark.asyncio()
+async def test_when_async_commands_timeout__they_should_be_removed_from_protocol_state(event_loop, with_server, advance_time):
+    imap_client = await login_user_async('user', 'pass', select=True, loop=event_loop)
+    await (imap_client.protocol.execute(Command(
+        'DELAY', imap_client.protocol.new_tag(), '3', loop=event_loop)))
+
+    noop_task = asyncio.ensure_future(imap_client.protocol.execute(
+        Command('NOOP', imap_client.protocol.new_tag(), '', loop=event_loop, timeout=2)))
+    await advance_time(1)
+
+    assert 1 == len(imap_client.protocol.pending_async_commands)
+    await advance_time(1.1)
+
+    finished, pending = await asyncio.wait([noop_task])
+    assert noop_task in finished
+    assert isinstance(noop_task.exception(), CommandTimeout)
+    assert 0 == len(imap_client.protocol.pending_async_commands)
+
+
+@pytest.mark.asyncio()
+async def test_when_sync_commands_timeout__they_should_be_removed_from_protocol_state(event_loop, with_server, advance_time):
+    imap_client = await login_user_async('user', 'pass', select=True, loop=event_loop)
+    await (imap_client.protocol.execute(Command(
+        'DELAY', imap_client.protocol.new_tag(), '3', loop=event_loop)))
+
+    delay_task = asyncio.ensure_future(imap_client.protocol.execute(
+        Command('DELAY', imap_client.protocol.new_tag(), '0', loop=event_loop, timeout=2)))
+    await advance_time(1)
+    assert imap_client.protocol.pending_sync_command is not None
+    await advance_time(1.1)
+
+    finished, pending = await asyncio.wait([delay_task])
+    assert delay_task in finished
+    assert isinstance(delay_task.exception(), CommandTimeout)
+    assert imap_client.protocol.pending_sync_command is None
+
+
+@pytest.mark.asyncio()
+async def test_idle_start__exits_queue_get_without_timeout_error(event_loop, with_server, advance_time):
+    imap_client = await login_user_async('user', 'pass', select=True, loop=event_loop)
+
+    idle_timeout = 5
+    await imap_client.idle_start(idle_timeout)
+
+    push_task = asyncio.ensure_future(imap_client.wait_server_push(idle_timeout + 2))
+    await advance_time(idle_timeout + 1)
+
+    r = await asyncio.wait_for(push_task, 0)
+    assert STOP_WAIT_SERVER_PUSH == r
+
+
+@pytest.mark.asyncio()
+async def test_idle_start__exits_queueget_with_keepalive_without_timeout_error(event_loop, with_server, advance_time):
+    imap_client = await login_user_async('user', 'pass', select=True, loop=event_loop)
+
+    # Idle long enough for the server to issue a keep-alive
+    server_idle_timeout = imapserver.ImapProtocol.IDLE_STILL_HERE_PERIOD_SECONDS
+    idle_timeout = server_idle_timeout + 1
+    idle = await imap_client.idle_start(idle_timeout)
+
+    push_task = asyncio.ensure_future(imap_client.wait_server_push(server_idle_timeout - 1))
+
+    # Advance time until we've received a keep-alive from server
+    await advance_time(server_idle_timeout)
+
+    # The original push task timed out
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(push_task, 0.1)
+
+    # Read the keepalive from the server
+    r = await imap_client.wait_server_push(0.1)
+    assert [b'OK Still here'] == r
+
+    # Advance the clock to the client timeout (idle waiter triggers)
+    await advance_time(1)
+    imap_client.idle_done()
+
+    r = await asyncio.wait_for(idle, 1)
+    assert "OK" == r.result
+
+    assert not imap_client.protocol._idle_event.is_set()
+
+    # Start another idle period
+    idle = await imap_client.idle_start(idle_timeout)
+    await advance_time(1)
+
+    # Read 'stop_wait_server_push'
+    push_task = asyncio.ensure_future(imap_client.wait_server_push(0.1))
+    await advance_time(1)
+    r = await asyncio.wait_for(push_task, None)
+    assert STOP_WAIT_SERVER_PUSH == r
+
+    # There shouldn't be anything left in the queue (no '+ idling')
+    with pytest.raises(asyncio.TimeoutError):
+        push_task = asyncio.ensure_future(imap_client.wait_server_push(0.1))
+        await advance_time(1)
+        await asyncio.wait_for(push_task, 0.1)
+
+    imap_client.idle_done()
+    await asyncio.wait_for(idle, 1)
+
